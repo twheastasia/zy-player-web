094a6286fba657bfcc441eb9ee51a0b4379c96d4
weihai.tang <weihai.tang@alo7.com>
2020-07-11 22:37:19 +0800

    fix warning

--- src/lib/element/index.js
+++ src/lib/element/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import { Message, Pagination } from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Pagination)
 
